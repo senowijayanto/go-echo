5b6d0e26b713497a9f06aa1cfbb6b61739d342a4
Seno Wijayanto <senowijayanto@gmail.com>
2019-07-29 10:40:10 +0700

    Delete models

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -11,70 +8,12 @@ import (
 )
 
 func main() {
-	var err error
-	db, err := sql.Open("postgres", "user=root password=secret dbname=books_database sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = db.Ping(); err != nil {
-		panic(err)
-	} else {
-		fmt.Println("DB Connected...")
-	}
-
 	// Echo instance
 	e := echo.New()
 
-	// GET all employee
-	e.GET("/employee", func(c echo.Context) error {
-		query := "SELECT id, name, salary, age FROM employees ORDER BY id"
-
-		employees := Employees{}
-
-		rows, err := db.Query(query)
-
-		if err != nil {
-			return err
-		}
-
-		defer rows.Close()
-
-		for rows.Next() {
-			employee := Employee{}
-
-			err := rows.Scan(&employee.ID, &employee.Name, &employee.Salary, &employee.Age)
-
-			if err != nil {
-				return err
-			}
-			employees = append(employees, employee)
-		}
-		return c.JSON(http.StatusOK, employees)
-	})
-
-	// GET employee by id
-	e.GET("/employee/:id", func(c echo.Context) error {
-		id := c.Param("id")
-
-		query := "SELECT id, name, salary, age FROM employees WHERE id = $1 ORDER BY id"
-
-		employee := Employee{}
-
-		statement, err := db.Prepare(query)
-
-		if err != nil {
-			return err
-		}
-
-		defer statement.Close()
-
-		err = statement.QueryRow(id).Scan(&employee.ID, &employee.Name, &employee.Salary, &employee.Age)
-
-		if err != nil {
-			return err
-		}
-		return c.JSON(http.StatusCreated, &employee)
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
 	})
 
 	// Start server
